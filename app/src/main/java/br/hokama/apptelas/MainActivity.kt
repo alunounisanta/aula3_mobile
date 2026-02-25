761b091fa07c54c4b391abb86e0e0d7d40195220
unisanta <unisanta@santacecilia.br>
2026-02-25 20:35:29 -0300

    Initial Commit

---
+++ app/src/main/java/br/hokama/apptelas/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.hokama.apptelas
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val fabAvanca = findViewById<FloatingActionButton>(R.id.fab_avanca)
+        fabAvanca.setOnClickListener{
+            val intent = Intent(this, SegundaActivity::class.java)
+            startActivity(intent)
+        }
+        //val fabVoltar = findViewById<FloatingActionButton>(R.id.fab_volta)
+
+    }
+}
---
+++ app/src/main/java/br/hokama/apptelas/SegundaActivity.kt
@@ -0,0 +1,26 @@
+package br.hokama.apptelas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val fabVolta = findViewById<FloatingActionButton>(R.id.fab_volta)
+        fabVolta.setOnClickListener{
+            finish()
+        }
+    }
+}
